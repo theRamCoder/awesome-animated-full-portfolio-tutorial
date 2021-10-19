75e9b929ee132ebc3b4148c9bbdfc039d6462c0f
ramCoder <90675423+theRamCoder@users.noreply.github.com>
2021-10-19 15:46:26 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var themeSwitcher = document.querySelector(".checkbox");
+
 var currentTheme = localStorage.getItem("theme");
 
 if (currentTheme) {
@@ -19,3 +20,21 @@ function switchTheme(e) {
 }
 
 themeSwitcher.addEventListener("change", switchTheme, false);
+
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".links");
+const links = document.querySelectorAll(".nav-link");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("active-nav");
+  burger.classList.toggle("fa-times");
+});
+
+for (let i of links) {
+  i.addEventListener("click", (e) => {
+    if (e.target.classList.contains("nav-link")) {
+      nav.classList.toggle("active-nav");
+      burger.classList.toggle("fa-times");
+    }
+  });
+}
